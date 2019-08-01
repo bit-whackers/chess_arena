6d124222ab6fa1c599ab8809b31546e1b5666509
Firstname Lastname <e-mail address>
2019-07-31 20:52:19 -0700

    set requirement for title input when create new game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,10 @@ class Game < ApplicationRecord
 	belongs_to :user
   has_many :pieces
   
+  validates :title,
+    presence: true,
+    length: {maximum: 255 , minimum: 3},
+    on: :create,
+    allow_nil: false
 
 end
