532e81651eb6921303281049a4daad621057fd5b
travis smith <travis86smith@gmail.com>
2019-08-14 04:09:23 +0000

    Removing Pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -57,5 +57,51 @@ def board_pieces
    Piece.create(game_id: id, piece_type: 'king', color: 'white', x_location: 4, y_location: 0)
    #king black
    Piece.create(game_id: id, piece_type: 'king', color: 'black', x_location: 3, y_location: 7)
+   #blank
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 0, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 1, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 2, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 3, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 4, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 5, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 6, y_location: 2)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 7, y_location: 2)
+
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 0, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 1, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 2, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 3, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 4, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 5, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 6, y_location: 3)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 7, y_location: 3)
+
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 0, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 1, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 2, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 3, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 4, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 5, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 6, y_location: 4)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 7, y_location: 4)
+
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 0, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 1, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 2, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 3, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 4, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 5, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 6, y_location: 5)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 7, y_location: 5)
+
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 0, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 1, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 2, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 3, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 4, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 5, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 6, y_location: 6)
+   Piece.create(game_id: id, piece_type: 'blank', color: 'black', x_location: 7, y_location: 6)
+
 end
 end
